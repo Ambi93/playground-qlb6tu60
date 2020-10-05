8dfd17f8eeddf108025fd69e1a93ddac26bd78da
stardisblue <stardisblue@users.noreply.github.com>
2020-10-05 11:24:40 +0200

    Update corrige.js

--- exercices/exo3/corrige.js
+++ exercices/exo3/corrige.js
@@ -1,33 +1,57 @@
-const $parole = document.getElementById('paroles');
-document
-  .getElementById('masquer-paroles')
-  .addEventListener('click', function () {
-    $parole.classList.toggle('hidden');
-  });
+const checkboxParoles = document.querySelector('#masquer-paroles');
+const checkboxLabel = checkboxParoles.parentElement
 
-const $refrains = [].slice.call(document.querySelectorAll('.refrain'), 1);
+const divParoles = document.querySelector('#paroles'); //
 
-document
-  .getElementById('masquer-refrains')
-  .addEventListener('click', function (ev) {
-    $refrains.forEach(function ($el) {
-      if (ev.target.checked) {
-        $el.querySelector('.definition').classList.remove('hidden');
-        $el.querySelector('.contenu').classList.add('hidden');
-      } else {
-        $el.querySelector('.definition').classList.add('hidden');
-        $el.querySelector('.contenu').classList.remove('hidden');
-      }
-    });
-  });
-
-$refrains.forEach(function (el) {
-  el.addEventListener('mouseenter', function () {
-    el.querySelector('.contenu').classList.remove('hidden');
-  });
-  el.addEventListener('mouseleave', function () {
-    if (!el.querySelector('.definition').classList.contains('hidden')) {
-      el.querySelector('.contenu').classList.add('hidden');
-    }
-  });
+checkboxParoles.addEventListener('click', function (event) {
+  if (event.target.checked) {
+      divParoles.classList.add('hidden');
+      checkboxLabel.lastChild.remove()
+      checkboxLabel.appendChild(document.createTextNode("Afficher les paroles"))
+  } else {
+    divParoles.classList.remove('hidden');
+    checkboxLabel.lastChild.remove()
+    checkboxLabel.appendChild(document.createTextNode("Masquer les paroles"))
+  }
 });
+
+
+const $checkboxRefrain = document.querySelector('#masquer-refrains')
+const $refrainLabel = $checkboxRefrain.parentElement
+
+const $refrains = document.querySelectorAll('.refrain')
+
+$checkboxRefrain.addEventListener('click', function(event){
+
+    $refrains.forEach(function($refrain, index){
+        if(index !== 0){
+            if (event.target.checked) {
+                $refrain.querySelector('.definition').classList.remove('hidden')
+                $refrain.querySelector('.contenu').classList.add('hidden')
+            } else {
+                $refrain.querySelector('.definition').classList.add('hidden')
+                $refrain.querySelector('.contenu').classList.remove('hidden')
+            }
+        }
+    })
+
+  if (event.target.checked) {
+    $refrainLabel.lastChild.remove()
+    $refrainLabel.appendChild(document.createTextNode("Afficher les paroles"))
+  } else {
+    $refrainLabel.lastChild.remove()
+    $refrainLabel.appendChild(document.createTextNode("Masquer les paroles"))
+  }
+})
+
+$refrains.forEach(function($refrain){
+    $refrain.addEventListener('mouseenter', function(){
+        $refrain.querySelector('.contenu').classList.remove('hidden')
+    })
+
+    $refrain.addEventListener('mouseleave', function(){
+        if(!$refrain.querySelector('.definition').classList.contains('hidden')){
+            $refrain.querySelector('.contenu').classList.add('hidden')
+        }
+    })
+})
